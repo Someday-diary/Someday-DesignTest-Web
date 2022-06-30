11acc1810f02f1a8c9100dd42bfb498c37803fb8
Junhong0209 <junh040209@naver.com>
2022-06-30 13:15:39 +0900

    chore(color): Add color to be used in the project
    프로젝트에 사용 되는 색상 추가

--- src/core/styles/color.ts
+++ src/core/styles/color.ts
@@ -1,6 +1,16 @@
 export const colors = {
   Black: "#000000",
   White: "#FFFFFF",
+  BGray13: "#1A2129",
+  BGray22: "#2B3845",
+  BGray30: "#3B4D5E",
+  BGray40: "#576675",
+  BGray56: "#7E8FA0",
+  BGray70: "#A3B3C2",
+  BGray82: "#C3D1DF",
+  BGray90: "#DDE6EE",
+  BGray94: "#EAF0F5",
+  BGray97: "#F3F8FC",
 
   ToastSuccess: "rgba(18, 24, 33, 08)",
   ToastError: "rgba(234, 31, 31, 0.8)",
